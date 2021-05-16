ff45fa153ec5f4cd014fa453fffbc5990e451932
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-05-16 17:20:30 +0900

    Add: 메타태그 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,8 +12,36 @@ export default class Doc extends Document {
             name="description"
             content="미래를 바꿀 디스코드 관리봇, Aztra"
           />
-          <link rel="apple-touch-icon" href="/logo192.png" />
           <link rel="manifest" href="/manifest.json" />
+
+
+          <meta name='application-name' content='Aztra' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Aztra' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='msapplication-TileColor' content='#4B4B78' />
+          <meta name='msapplication-tap-highlight' content='no' />
+
+          <link rel='apple-touch-icon' href='/logo192.png' />
+
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='shortcut icon' href='/favicon.ico' />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' />
+
+          <meta name='twitter:card' content='summary' />
+          <meta name='twitter:url' content='https://aztra.xyz' />
+          <meta name='twitter:title' content='Aztra' />
+          <meta name='twitter:description' content='미래를 바꿀 디스코드 관리봇, Aztra' />
+          <meta name='twitter:image' content='https://aztra.xyz/logo192.png' />
+          <meta property='og:type' content='website' />
+          <meta property='og:title' content='Aztra' />
+          <meta property='og:description' content='미래를 바꿀 디스코드 관리봇, Aztra' />
+          <meta property='og:site_name' content='Aztra' />
+          <meta property='og:url' content='https://aztra.xyz' />
+          <meta property='og:image' content='https://aztra.xyz/logo192.png' />
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
