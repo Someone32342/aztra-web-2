2a075182c2c7f5142a6c0d5dcb1f816b59ede539
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-07-05 00:01:38 +0900

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -154,6 +154,7 @@ export default function Sidebar(props: SidebarProps) {
               </div>
               <div>
                 자동 작업
+                <Badge variant="warning" className="ml-2 my-auto" style={{ fontSize: 14 }}>점검중</Badge>
               </div>
             </Nav.Link>
           </Link>
