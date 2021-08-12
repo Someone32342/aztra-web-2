059d0f7a4c57c94d5ebeb8d3edf50f1963c7cabe
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-08-12 22:35:12 +0900

    Add: Prettier

--- pages/dashboard/[guildid]/security.tsx
+++ pages/dashboard/[guildid]/security.tsx
@@ -1,32 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Spinner, Container, Form, Col, Badge, Button } from 'react-bootstrap'
-import Cookies from 'universal-cookie'
+import React, { useEffect } from 'react';
+import {
+  Row,
+  Spinner,
+  Container,
+  Form,
+  Col,
+  Badge,
+  Button,
+} from 'react-bootstrap';
+import Cookies from 'universal-cookie';
 import Layout from 'components/Layout';
 import DashboardLayout from 'components/DashboardLayout';
 import Head from 'next/head';
 import { GetServerSideProps, NextPage } from 'next';
 
 interface SecurityRouterProps {
-  guildId: string
+  guildId: string;
 }
 
-export const getServerSideProps: GetServerSideProps<SecurityRouterProps> = async context => {
-  const { guildid } = context.query
-  return {
-    props: {
-      guildId: guildid as string
-    }
-  }
-}
+export const getServerSideProps: GetServerSideProps<SecurityRouterProps> =
+  async (context) => {
+    const { guildid } = context.query;
+    return {
+      props: {
+        guildId: guildid as string,
+      },
+    };
+  };
 
 const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
   useEffect(() => {
     if (!new Cookies().get('ACCESS_TOKEN')) {
-      const lct = window.location
-      localStorage.setItem('loginFrom', lct.pathname + lct.search)
-      window.location.assign('/login')
+      const lct = window.location;
+      localStorage.setItem('loginFrom', lct.pathname + lct.search);
+      window.location.assign('/login');
     }
-  }, [])
+  }, []);
 
   return (
     <>
@@ -35,14 +44,15 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
       </Head>
       <Layout>
         <DashboardLayout guildId={guildId}>
-          {
-            () => true ? (
+          {() =>
+            true ? (
               <div>
                 <Row className="dashboard-section">
                   <div>
                     <h3>보안 설정</h3>
                     <div className="py-2">
-                      여러분의 서버를 악의적인 공격으로부터 보호할 기능들을 제공합니다.
+                      여러분의 서버를 악의적인 공격으로부터 보호할 기능들을
+                      제공합니다.
                     </div>
                   </div>
                 </Row>
@@ -50,7 +60,12 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
                 <Row className="pb-3 align-items-center">
                   <div>
                     <h4>멤버 초대 보안</h4>
-                    <small>추가 인증이 적용된 초대 링크를 발급하여 악성 멤버가 참여하는 것을 막습니다. 기존 디스코드 초대 링크에는 이 기능이 적용되지 않으니 반드시 아래의 자체 초대 링크를 사용하세요!</small>
+                    <small>
+                      추가 인증이 적용된 초대 링크를 발급하여 악성 멤버가
+                      참여하는 것을 막습니다. 기존 디스코드 초대 링크에는 이
+                      기능이 적용되지 않으니 반드시 아래의 자체 초대 링크를
+                      사용하세요!
+                    </small>
                   </div>
                 </Row>
 
@@ -76,13 +91,15 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
                         placeholder="https://aztra.xyz/invite/A2Fg15Gvx6"
                       />
                       <small>
-                        <Badge variant="aztra" className="ml-2">PRO</Badge> Aztra Pro로 업그레이드하면 커스텀 링크를 사용할 수 있습니다!
+                        <Badge variant="aztra" className="ml-2">
+                          PRO
+                        </Badge>{' '}
+                        Aztra Pro로 업그레이드하면 커스텀 링크를 사용할 수
+                        있습니다!
                       </small>
                     </Col>
                     <Col xs="auto">
-                      <Button variant="aztra">
-                        복사하기
-                      </Button>
+                      <Button variant="aztra">복사하기</Button>
                     </Col>
                   </Row>
                   <Row className="pb-3">
@@ -91,24 +108,35 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
                         id="by-naver-oauth"
                         custom
                         type="checkbox"
-                        label={<>네이버 아이디로 인증<Badge variant="aztra" className="ml-2">PRO</Badge></>}
+                        label={
+                          <>
+                            네이버 아이디로 인증
+                            <Badge variant="aztra" className="ml-2">
+                              PRO
+                            </Badge>
+                          </>
+                        }
                       />
                     </Col>
                   </Row>
                 </Form.Group>
               </div>
-            )
-              : <Container className="d-flex align-items-center justify-content-center flex-column" style={{
-                height: '500px'
-              }}>
+            ) : (
+              <Container
+                className="d-flex align-items-center justify-content-center flex-column"
+                style={{
+                  height: '500px',
+                }}
+              >
                 <h3 className="pb-4">불러오는 중</h3>
                 <Spinner animation="border" variant="aztra" />
               </Container>
+            )
           }
         </DashboardLayout>
       </Layout>
     </>
-  )
-}
+  );
+};
 
-export default Security
+export default Security;
