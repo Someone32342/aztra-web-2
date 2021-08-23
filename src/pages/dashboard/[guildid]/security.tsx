3cb02b90d936f2d1bcae30750fd0a92dee94957e
ArpaAP <54466872+ArpaAP@users.noreply.github.com>
2021-08-23 21:18:54 +0900

    Add: 해외 IP 차단 및 예외 국가 설정 ui 추가

--- src/pages/dashboard/[guildid]/security.tsx
+++ src/pages/dashboard/[guildid]/security.tsx
@@ -11,6 +11,7 @@ import {
   Overlay,
   Tooltip,
   Card,
+  Dropdown,
 } from 'react-bootstrap';
 import Cookies from 'universal-cookie';
 import Layout from 'components/Layout';
@@ -20,7 +21,7 @@ import { GetServerSideProps, NextPage } from 'next';
 import {
   Add as AddIcon,
   Check as CheckIcon,
-  Edit as EditIcon,
+  Clear as ClearIcon,
   RemoveCircleOutline as RemoveCircleOutlineIcon,
   FileCopyOutlined as FileCopyOutlinedIcon,
 } from '@material-ui/icons';
@@ -48,6 +49,9 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
   const [newInvite, setNewInvite] = useState(false);
   const [newValidity, setNewValidity] = useState(0);
   const [newMaxUses, setNewMaxUses] = useState(0);
+  const [useBlockForeign, setUseBlockForeign] = useState(false);
+  const [blockedForeigns, setBlockedForeigns] = useState(new Set<string>());
+  const [searchCountry, setSearchCountry] = useState('');
   const [copied, setCopied] = useState(false);
   const copyButtonRef = useRef<HTMLButtonElement>(null);
 
@@ -287,6 +291,119 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
                       </Col>
                     </Row>
 
+                    <Row>
+                      <Col xs={12}>
+                        <Form.Check
+                          id="block-foreign-ip"
+                          custom
+                          checked={useBlockForeign}
+                          onChange={() => setUseBlockForeign(!useBlockForeign)}
+                          type="checkbox"
+                          label="해외 IP 차단하기"
+                        />
+
+                        <div className="pl-3 pt-2">
+                          {/*
+                            <Form.Control as="select">
+                              <option value={0}>예외 국가 선택...</option>
+                              <Countries />
+                            </Form.Control>
+                          */}
+
+                          <Dropdown
+                            className="dropdown-menu-dark"
+                            onSelect={(e) => {
+                              blockedForeigns.add(e!);
+                              setBlockedForeigns(new Set(blockedForeigns));
+                            }}
+                            onToggle={(isOpen) => {
+                              isOpen && setSearchCountry('');
+                            }}
+                          >
+                            <Dropdown.Toggle
+                              id="select-country"
+                              variant="outline-light"
+                              size="sm"
+                              className="my-1 remove-after d-flex align-items-cente shadow-none"
+                            >
+                              예외 국가 추가
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu
+                              className="bg-dark"
+                              style={{
+                                maxHeight: 300,
+                                minWidth: 240,
+                                overflowY: 'scroll',
+                              }}
+                            >
+                              <Form.Control
+                                id="add-role-member-search"
+                                className="mb-2"
+                                type="text"
+                                placeholder="국가 검색..."
+                                autoComplete="off"
+                                value={searchCountry}
+                                onChange={(e) =>
+                                  setSearchCountry(e.target.value)
+                                }
+                              />
+                              {COUNTRIES.filter(
+                                (one) =>
+                                  one[0].startsWith(
+                                    searchCountry.normalize().toUpperCase()
+                                  ) || one[1].includes(searchCountry)
+                              ).map((one) => (
+                                <Dropdown.Item
+                                  className="my-1"
+                                  key={one[0]}
+                                  eventKey={one[0]}
+                                  active={false}
+                                >
+                                  {one[1]}
+                                </Dropdown.Item>
+                              ))}
+                            </Dropdown.Menu>
+                          </Dropdown>
+
+                          {!!blockedForeigns.size && (
+                            <div className="pt-2">
+                              {[...blockedForeigns].map((one) => (
+                                <Badge
+                                  key={one}
+                                  className="d-inline-flex align-items-center mr-2 mb-2"
+                                  variant="secondary"
+                                  style={{ fontSize: 15 }}
+                                >
+                                  {COUNTRIES.find((c) => c[0] === one)![1]}{' '}
+                                  <ClearIcon
+                                    className="ml-1 cursor-pointer"
+                                    onClick={() => {
+                                      blockedForeigns.delete(one);
+                                      setBlockedForeigns(
+                                        new Set(blockedForeigns)
+                                      );
+                                    }}
+                                    style={{ fontSize: 16 }}
+                                  />
+                                </Badge>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                      </Col>
+                    </Row>
+
+                    <Row className="pt-2 pb-3">
+                      <Col xs={12}>
+                        <Form.Check
+                          id="block-vpn"
+                          custom
+                          type="checkbox"
+                          label="VPN 차단하기"
+                        />
+                      </Col>
+                    </Row>
+
                     <Row className="pb-3">
                       <Col xs={12}>
                         <Form.Check
@@ -521,5 +638,257 @@ const Security: NextPage<SecurityRouterProps> = ({ guildId }) => {
     </>
   );
 };
+
+const COUNTRIES = [
+  ['GH', '가나'],
+  ['GA', '가봉'],
+  ['GY', '가이아나'],
+  ['GM', '감비아'],
+  ['GG', '건지섬'],
+  ['GP', '과달루페'],
+  ['GT', '과테말라'],
+  ['GU', '괌'],
+  ['VA', '교황청'],
+  ['GD', '그레나다'],
+  ['GE', '그루지야'],
+  ['GR', '그리스'],
+  ['GL', '그린랜드'],
+  ['GW', '기네비소'],
+  ['GN', '기니'],
+  ['ZY', '기타주소'],
+  ['NA', '나미비아'],
+  ['NR', '나우루 공화국'],
+  ['NG', '나이지리아'],
+  ['SS', '남수단'],
+  ['ZA', '남아프리카공화국'],
+  ['NL', '네덜란드'],
+  ['NP', '네팔'],
+  ['NO', '노르웨이'],
+  ['NF', '노퍽제도'],
+  ['NC', '뉴 켈레도니아'],
+  ['NZ', '뉴질랜드'],
+  ['NU', '니우에'],
+  ['NE', '니제르'],
+  ['NI', '니카라과'],
+  ['TW', '대만'],
+  ['KR', '대한민국'],
+  ['DK', '덴마크'],
+  ['DM', '도미니카'],
+  ['DO', '도미니카 공화국'],
+  ['DE', '독일'],
+  ['LA', '라오스'],
+  ['LR', '라이베리아'],
+  ['LV', '라트비아'],
+  ['RU', '러시아'],
+  ['LB', '레바논'],
+  ['LS', '레소토'],
+  ['RO', '루마니아'],
+  ['LU', '룩셈부르크 대공화국'],
+  ['RW', '르완다'],
+  ['LY', '리비아 인민사회주의공화국'],
+  ['LT', '리투아니아'],
+  ['LI', '리히텐슈타인'],
+  ['MG', '마다가스카르'],
+  ['MQ', '마르티니크'],
+  ['MH', '마쉘 아일랜드'],
+  ['YT', '마요트'],
+  ['FM', '마이크로네시아'],
+  ['MO', '마카오'],
+  ['MK', '마케도니아'],
+  ['MW', '말라위'],
+  ['MY', '말레이지아'],
+  ['ML', '말리'],
+  ['IM', '맨섬'],
+  ['MX', '멕시코'],
+  ['MC', '모나코'],
+  ['MA', '모로코'],
+  ['MU', '모리셔스'],
+  ['MR', '모리타니'],
+  ['MZ', '모잠비크'],
+  ['ME', '몬테네그로'],
+  ['MS', '몬트세라트'],
+  ['MD', '몰도바'],
+  ['MV', '몰디브'],
+  ['MT', '몰타'],
+  ['MN', '몽골'],
+  ['US', '미국'],
+  ['VI', '미국령 버진제도'],
+  ['AS', '미국령 사모아'],
+  ['UM', '미국령 소군도'],
+  ['MM', '미얀마 연방'],
+  ['ZZ', '미할당'],
+  ['VU', '바누아투'],
+  ['BH', '바레인'],
+  ['BB', '바베이도스'],
+  ['BS', '바하마'],
+  ['BD', '방글라데시'],
+  ['BM', '버뮤다'],
+  ['VE', '베네수엘라'],
+  ['BJ', '베넹'],
+  ['VN', '베트남'],
+  ['BE', '벨기에'],
+  ['BY', '벨로루시'],
+  ['BZ', '벨리즈'],
+  ['BQ', '보네르섬'],
+  ['BA', '보스니아-헤르체고비나'],
+  ['BW', '보츠와나'],
+  ['BO', '볼리비아'],
+  ['BF', '부루키나파소'],
+  ['BI', '부룬디'],
+  ['BT', '부탄'],
+  ['MP', '북 마리아나 제도'],
+  ['BG', '불가리아'],
+  ['BR', '브라질'],
+  ['BN', '브루나이'],
+  ['WS', '사모아'],
+  ['SA', '사우디 아라비아'],
+  ['SM', '산 마리노'],
+  ['ST', '상토메 프린시페'],
+  ['BL', '생바르텔레미'],
+  ['EH', '서 사하라'],
+  ['SN', '세네겔'],
+  ['RS', '세르비아'],
+  ['CS', '세르비아 몬테네그로'],
+  ['SC', '세이셀'],
+  ['PM', '세이트 피에레 앤 미켈론'],
+  ['KN', '세인트 키트 앤 네비스'],
+  ['SH', '세인트 헬레나'],
+  ['MF', '세인트마틴'],
+  ['LC', '센인트 루시아'],
+  ['VC', '센인트 빈센트와 그레나다'],
+  ['SO', '소말리아'],
+  ['SB', '솔로몬 제도'],
+  ['SD', '수단'],
+  ['SR', '수리남'],
+  ['LK', '스리랑카'],
+  ['SZ', '스와질랜드'],
+  ['SE', '스웨덴'],
+  ['CH', '스위스'],
+  ['ES', '스페인'],
+  ['SK', '슬로바키아'],
+  ['SI', '슬로베니아'],
+  ['SY', '시리아'],
+  ['SL', '시에라 리온'],
+  ['SX', '신트마르턴'],
+  ['SG', '싱가폴'],
+  ['AE', '아랍에미리트 연합국'],
+  ['AW', '아루바'],
+  ['AM', '아르메니아'],
+  ['AR', '아르헨티나'],
+  ['AP', '아시아태평양 주소관리기구'],
+  ['IS', '아이슬랜드'],
+  ['HT', '아이티'],
+  ['IE', '아일랜드'],
+  ['AZ', '아제르바이잔'],
+  ['AF', '아프가니스탄'],
+  ['AD', '안도라'],
+  ['AL', '알바니아'],
+  ['DZ', '알제리'],
+  ['AO', '앙골라'],
+  ['AI', '앙귈라'],
+  ['AN', '앤틸리스 열도'],
+  ['ER', '에리트레아'],
+  ['EE', '에스토니아'],
+  ['EC', '에콰도로'],
+  ['ET', '에티오피아'],
+  ['AG', '엔티가 바부다'],
+  ['SV', '엘 살바도르'],
+  ['IO', '영구령 인도양'],
+  ['UK', '영국'],
+  ['GB', '영국'],
+  ['VG', '영국령 버진제도'],
+  ['YE', '예멘'],
+  ['OM', '오만'],
+  ['AU', '오스트레일리아'],
+  ['AT', '오스트리아'],
+  ['HN', '온두라스'],
+  ['AX', '욀란드'],
+  ['JO', '요르단'],
+  ['UG', '우간다'],
+  ['UY', '우루과이'],
+  ['UZ', '우즈베키스탄'],
+  ['UA', '우크라이나'],
+  ['WF', '웰 앤 퓨투나 제도'],
+  ['YU', '유고슬라비아'],
+  ['EU', '유럽연합'],
+  ['IQ', '이라크'],
+  ['IR', '이란'],
+  ['IL', '이스라엘'],
+  ['EG', '이집트'],
+  ['IT', '이탈리아'],
+  ['IN', '인도'],
+  ['ID', '인도네시아'],
+  ['JP', '일본'],
+  ['JM', '자메이카'],
+  ['ZM', '잠비아'],
+  ['JE', '저지섬'],
+  ['GQ', '적도 기네'],
+  ['KP', '조선민주주의인민공화국'],
+  ['CN', '중국'],
+  ['CF', '중앙 아프리카 공화국'],
+  ['DJ', '지부티'],
+  ['GI', '지브롤터'],
+  ['ZW', '짐바브웨'],
+  ['TD', '차드'],
+  ['CZ', '체코 공화국'],
+  ['CL', '칠레'],
+  ['CM', '카메룬'],
+  ['CV', '카보 베르데'],
+  ['KZ', '카자흐스탄'],
+  ['QA', '카타르'],
+  ['KH', '캄보디아'],
+  ['CA', '캐나다'],
+  ['KE', '케냐'],
+  ['KY', '케이맨제도'],
+  ['KM', '코모로 이슬람연방공화국'],
+  ['CR', '코스타리카'],
+  ['CI', '코트 디브와르'],
+  ['CO', '콜롬비아'],
+  ['CD', '콩고공화국'],
+  ['CG', '콩코'],
+  ['CU', '쿠바'],
+  ['KW', '쿠웨이트'],
+  ['CK', '쿡제도'],
+  ['CW', '퀴라소'],
+  ['HR', '크로아티아'],
+  ['KG', '키리기스스탄'],
+  ['KI', '키리바시'],
+  ['CY', '키프러스'],
+  ['TH', '타이'],
+  ['TJ', '타지키스탄'],
+  ['TZ', '탄자니아'],
+  ['TC', '터기 앤 카이코스 제도'],
+  ['TR', '터어키 공화국'],
+  ['TG', '토고'],
+  ['TK', '토켈라우 제도'],
+  ['TO', '통가'],
+  ['TM', '투르크메니스탄'],
+  ['TV', '투발루'],
+  ['TN', '튀니지'],
+  ['TT', '트리니다드 토바고'],
+  ['TL', '티모르 레스테'],
+  ['PA', '파나마 공화국'],
+  ['PY', '파라과이'],
+  ['PK', '파키스탄'],
+  ['PG', '파푸아 뉴기아'],
+  ['PW', '팔라우'],
+  ['PS', '팔레스타인'],
+  ['FO', '페로스 제도'],
+  ['PE', '페루'],
+  ['PT', '포루투갈'],
+  ['FK', '포클랜드 제도'],
+  ['PL', '폴란드'],
+  ['PR', '푸에르토 리코'],
+  ['FR', '프랑스'],
+  ['GF', '프랑스령 기아나'],
+  ['PF', '프랑스령 폴리네시아'],
+  ['RE', '프랑스령리유니온'],
+  ['FJ', '피지'],
+  ['PN', '피트케언'],
+  ['FI', '핀란드'],
+  ['PH', '필리핀'],
+  ['HU', '헝가리'],
+  ['HK', '홍콩'],
+];
 
 export default Security;
